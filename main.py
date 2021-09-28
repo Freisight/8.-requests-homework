a88b1ba38994236fb03f7a43b0f9487c1c7bec2b
Freisight <akagemor@list.ru>
2021-09-28 23:43:39 +0300

    first exersice ended

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+# задача 1
+
 import requests
 TOKEN = '2619421814940190'
-url_hero = f'https://www.superheroapi.com/api.php/{TOKEN}/search/Thanos'
 
-req = requests.get(url_hero).json()
-results = req['results']
+url_heroes = [
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Hulk',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Thanos',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Captain%America'
+    ]
 
-for item in results:
-    # print(item)
-    for keys, values in item.items():
-        # print(keys, values)
-        if keys == 'powerstats':
-            print(values['intelligence'])
-            break
+
+
+all_chars = {}
+for chars in url_heroes:
+    req = requests.get(chars).json()
+    results = req['results']
+    for item in results:
+        for keys, values in item.items():
+            if keys == 'powerstats':
+                all_chars[item['name']] = int(values['intelligence'])
+
+print(all_chars)
+
+sorted_chars = dict(sorted(all_chars.items(), key=lambda x: x[1]))
+print(f'Самый умный герой {max(sorted_chars)}, у него {max(sorted_chars.values())} интеллекта!')
+
+# задача 2
+
